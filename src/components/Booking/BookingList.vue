a5ec2d550a35e2f7ec49ff7cd02996b514065c2a
Mubashar Shahzad <mubashar.shahzad23@gmail.com>
2019-12-06 02:52:50 +0200

    sweet alert and modal

--- src/components/Booking/BookingList.vue
+++ src/components/Booking/BookingList.vue
@@ -22,17 +22,58 @@
                 <td>{{booking.parking_id}}</td> <!-- get parking object with it, to show parking place name -->
                 <td>{{booking.status}}</td>
                 <td>
-                    <button type="button" @click="btnExtendClick(booking.id)" class=" btn btn-sm btn-success">Extend</button>
-                    <button type="button" @click="btnCancelClick(booking.id)" class=" btn btn-sm btn-danger">Cancel</button>
+                    <!-- <button type="button" @click="btnExtendClick(booking.id)" class=" btn btn-sm btn-success">Extend</button> -->
+                    <!-- <button type="button" @click="btnCancelClick(booking.id)" class=" btn btn-sm btn-danger">Cancel</button> -->
+                    <button type="button" @click="btnExtendClick(booking)" class=" btn btn-sm btn-success" data-toggle="modal" data-target="#extendModal">Extend</button>
+                    <button type="button" v-on:click="btnCancelClick(booking)" class=" btn btn-sm btn-danger">Cancel</button>
                 </td>
             </tr>
         </tbody>
-    </table>   
+    </table> 
+        <!-- Modal -->
+    <div id="extendModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">Extend Booking</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="usr">Extend Time:</label>
+                    <!-- <date-picker v-model="ExtendTime"></date-picker> -->
+                    <input type="text" class="form-control" id="usr">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-success">
+                    <span class="glyphicon glyphicon-floppy-disk"></span> Save
+                </button>
+            </div>
+            </div>
+
+        </div>
+    </div>  
 </div>
 </template>
 
 <script>
+// import moment from 'moment';
+// import datePicker from 'vue-bootstrap-datetimepicker';
+// // You have to add CSS yourself
+// import 'eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.css';
+// // Bootstrap css  
+// import 'bootstrap/dist/css/bootstrap.css';
+
+import Vue from 'vue'
+import VueSweetAlert from 'vue-sweetalert'
+
 import axios from 'axios'
+
+Vue.use(VueSweetAlert)
 
 export default {
   name: 'BookingList',
@@ -46,7 +87,8 @@ export default {
         //     user_id: 0,
         //     parking_id: 0
         // },
-        bookingList: []
+        // ExtendTime: new Date(),
+        bookingList: [],
     }
   },
   async created () {
@@ -58,23 +100,55 @@ export default {
       }
   },
   methods: {
-      btnExtendClick (id) {
-          alert('Extend: '+id);
+      btnExtendClick (bookingObj) {
+          console.log("bookingObj",bookingObj);
       },
-      async btnCancelClick(id){
-          try {
-              const {data} = await axios.post('http://localhost:4000/api/bookings/cancel', {id: id}, { headers: {"Authorization" : `Bearer ${localStorage.getItem('token')}`}})
-          } catch(e) {
-              alert('Error:'+ e)
-          }
-          
-          try {
-              const {data} = await axios.get('http://localhost:4000/api/bookings/all', { headers: {"Authorization" : `Bearer ${localStorage.getItem('token')}`}})
-              this.bookingList = data
-          } catch (e) {
-              console.log(e)
-          }
+      btnCancelClick(bookingObj){
+          this.$swal(
+              {
+                    title: 'Are you sure?',
+                    type: "warning",
+                    showCancelButton: true,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "Yes, cancel it!",
+                    cancelButtonText: "No, keep safe!"
+              }, 
+                async function(isConfirm) {
+                    if (isConfirm) {
+                        // handle confirm
+                        try {
+                            const {data} = await axios.post('http://localhost:4000/api/bookings/cancel', {id: bookingObj.id}, { headers: {"Authorization" : `Bearer ${localStorage.getItem('token')}`}})
+                        } catch(e) {
+                            alert('Error:'+ e)
+                        }
+                        
+                        try {
+                            const {data} = await axios.get('http://localhost:4000/api/bookings/all', { headers: {"Authorization" : `Bearer ${localStorage.getItem('token')}`}})
+                            this.bookingList = data
+                        } catch (e) {
+                            console.log(e)
+                        }
+                    } else {
+                        this.$swal("Cancelled", "Your booking is saved back to your list", "error");
+                        // handle all other cases
+                    }
+                }
+          )
       }
+    //   async btnCancelClick(id){
+    //       try {
+    //           const {data} = await axios.post('http://localhost:4000/api/bookings/cancel', {id: id}, { headers: {"Authorization" : `Bearer ${localStorage.getItem('token')}`}})
+    //       } catch(e) {
+    //           alert('Error:'+ e)
+    //       }
+          
+    //       try {
+    //           const {data} = await axios.get('http://localhost:4000/api/bookings/all', { headers: {"Authorization" : `Bearer ${localStorage.getItem('token')}`}})
+    //           this.bookingList = data
+    //       } catch (e) {
+    //           console.log(e)
+    //       }
+    //   }
   }
 }
 </script>
